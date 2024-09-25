cc73a11763a8120acca48104798725c15ca2cf88
MangoSteen0903 <sjlee990129@gmail.com>
2024-09-25 11:36:22 +0900

    fix: change kalosm to async-openai

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use kalosm::language::*;
+use async_openai::types::{CreateCompletionRequest, CreateCompletionRequestArgs};
+
 // TODO: OPENAI_API_KEY (https://platform.openai.com/account/api-keys) 환경변수 설정
 #[tokio::main]
 async fn main() {
-    let llm = Gpt3_5::default();
-    let prompt = "Write 300 word essay of shoegaze which is music genre.";
-
-    let mut stream = llm
-        .stream_text(prompt)
-        .with_max_length(1000)
+    let client = async_openai::Client::new();
+    let request = CreateCompletionRequestArgs::default()
+        .model("gpt-4o")
+        .prompt("슈게이징 장르를 설명해줘")
+        .max_tokens(40_u32)
+        .build()
+        .expect("Fail to request to gpt");
+    let response = client
+        .completions()
+        .create(request)
         .await
-        .expect("Fail to get result");
-    stream
-        .to_std_out()
-        .await
-        .expect("Fail to write result on stdout");
+        .expect("Fail to complete result");
+    for choice in response.choices {
+        println!("{}", choice.text);
+    }
 }
